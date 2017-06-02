b04372f903b6cbb290fc4db548c23f0d30f20a66
Jim Noellsch <jnoellsch@rightpoint.com>
2017-06-02 16:45:32 -0500

    Added fail fast detection for non-updated settings.

--- Src/CognitiveMedia/SitecoreSettingsManager.cs
+++ Src/CognitiveMedia/SitecoreSettingsManager.cs
@@ -1,7 +1,29 @@
 ﻿namespace Sitecore.SharedSource.CognitiveMedia
 {
+    using System;
+
     public class SitecoreSettingsManager : IApiKeySettings
     {
-        public string ComputerVision => Configuration.Settings.GetSetting("CognitiveMedia.ApiKey.ComputerVision");
+        private const string DefaultValue = "{YOUR_API_KEY}";
+
+        public SitecoreSettingsManager()
+        {
+            this.ComputerVision = this.TryGetSetting("CognitiveMedia.ApiKey.ComputerVision");
+        }
+
+        public string ComputerVision { get; }
+
+        private string TryGetSetting(string key)
+        {
+            string val = Configuration.Settings.GetSetting(key);
+            bool isDefaultOrMissing = val.Equals(DefaultValue, StringComparison.OrdinalIgnoreCase) || string.IsNullOrWhiteSpace(val);
+
+            if (isDefaultOrMissing)
+            {
+                throw new Exception($"The value for the {key} setting is still set to the default value or is missing. Do you need to patch the setting?");
+            }
+
+            return val;
+        }
     }
 }
